0738fea423a27379d7b18f7665ddaf6451d85a97
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-03-26 17:24:44 -0300

    signUp - inicializa e seta username em database

--- src/script/auth.js
+++ src/script/auth.js
@@ -1,27 +1,52 @@
 var config = {
-    apiKey: "AIzaSyC4-oq1Sj96_IJvy_wFxF7DoJRzDO5Lcks",
-    authDomain: "educalab-52ee0.firebaseapp.com",
-    databaseURL: "https://educalab-52ee0.firebaseio.com",
-    projectId: "educalab-52ee0",
-    storageBucket: "educalab-52ee0.appspot.com",
-    messagingSenderId: "503327975641"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyC4-oq1Sj96_IJvy_wFxF7DoJRzDO5Lcks",
+  authDomain: "educalab-52ee0.firebaseapp.com",
+  databaseURL: "https://educalab-52ee0.firebaseio.com",
+  projectId: "educalab-52ee0",
+  storageBucket: "educalab-52ee0.appspot.com",
+  messagingSenderId: "503327975641"
+};
+firebase.initializeApp(config);
 
-  function signUp(email, password) {
-    firebase.auth().createUserWithEmailAndPassword(email, password).then(function (){
-        console.log("oi");
-        // window.location = 'timeline.html'
+// signUp
+var database = firebase.database();
+
+function signUp(email, password) {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function (response) {
+      console.log("oi");
+      const userId = response.user.uid
+      database.ref('users/' + userId).set({
+        username: name,
+        email: email
+      });
+      console.log(response)
+      // window.location = 'timeline.html'
     })
-    .catch(function(error) {
-         var errorCode = error.code;
-         var errorMessage = error.message;
-        //  if (errorCode == 'auth/weak-password') {
-        //    alert('The password is too weak.');
-        //  } else {
-        //    alert(errorMessage);
-        //  }
-         console.log(errorCode, errorMessage);
-    });
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      //  if (errorCode == 'auth/weak-password') {
+      //    alert('The password is too weak.');
+      //  } else {
+      //    alert(errorMessage);
+      //  }
+      console.log(errorCode, errorMessage);
+    }); 
 
-  }
+
+    // ref("user/${uid}").set({})
+
+
+
+}
+
+// DataBase
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture: imageUrl
+  });
+}
--- src/script/main.js
+++ src/script/main.js
@@ -1,10 +1,10 @@
+
 $(document).ready(function getEmailAndPassword(){
     $('#btn-sign-up').click(function (event){
         event.preventDefault()
-
         const email = $('#email').val();
         const password = $('#password').val();
-
-        signUp(email, password);
+        const name = $('#name').val();
+        signUp(email, password, name);
     })
 })
