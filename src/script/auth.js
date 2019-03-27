4722a94207cf022cca537d3b4459f0497308ab50
Viviane <viviane.a.gaspar@gmail.com>
2019-03-27 20:51:16 -0300

    post - Inclui chave firebase no config.js e altera a estrutura de arquivos

--- src/script/auth.js
+++ src/script/auth.js
@@ -1,13 +1,3 @@
-var config = {
-  apiKey: "AIzaSyC4-oq1Sj96_IJvy_wFxF7DoJRzDO5Lcks",
-  authDomain: "educalab-52ee0.firebaseapp.com",
-  databaseURL: "https://educalab-52ee0.firebaseio.com",
-  projectId: "educalab-52ee0",
-  storageBucket: "educalab-52ee0.appspot.com",
-  messagingSenderId: "503327975641"
-};
-firebase.initializeApp(config);
-
 // signUp
 var database = firebase.database();
 
