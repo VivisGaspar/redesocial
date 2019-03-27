979b4958ee4b7bcd54055c7ce8cf6666461ae602
marinakeu <marina_keunecke@hotmail.com>
2019-03-27 17:27:20 -0300

    signUp - inclui em signIn e signUp tratamento de erros padrão do firebase

--- src/script/auth.js
+++ src/script/auth.js
@@ -26,11 +26,11 @@ function signUp(email, password) {
   .catch(function (error) {
     var errorCode = error.code;
     var errorMessage = error.message;
-    //  if (errorCode == 'auth/weak-password') {
-    //    alert('The password is too weak.');
-    //  } else {
-    //    alert(errorMessage);
-    //  }
+     if (errorCode == 'auth/weak-password') {
+       alert('The password is too weak.');
+     } else {
+       alert(errorMessage);
+     }
     console.log(errorCode, errorMessage);
   }); 
   
@@ -49,11 +49,11 @@ function signIn(emailSignIn, passwordSignIn) {
   .catch(function (error) {
     var errorCode = error.code;
     var errorMessage = error.message;
-    //  if (errorCode == 'auth/weak-password') {
-    //    alert('The password is too weak.');
-    //  } else {
-    //    alert(errorMessage);
-    //  }
+     if (errorCode == 'auth/weak-password') {
+       alert('The password is too weak.');
+     } else {
+       alert(errorMessage);
+     }
     console.log(errorCode, errorMessage);
 
   }); 
