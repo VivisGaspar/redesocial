115f6a352a15896ff0daf5a7f2858e4a0a5ec6ae
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-04-09 17:17:59 -0300

    edit-profile-bootstrap - implementa estilo na página de edição de perfil

--- public/src/script/profile.js
+++ public/src/script/profile.js
@@ -1,9 +1,10 @@
 function getInfoEditProfile(nameEdit, lastNameEdit, turma) {
     $("#new-profile").prepend(`
-    <h1>Dados alterados com sucesso:</h1>
+    <h3>Dados alterados com sucesso:</h3>
         <p>Nome: ${nameEdit}</p>
         <p>Sobrenome: ${lastNameEdit}</p>
-        <p>Turma: ${turma}</p>`)
+        <p>Turma: ${turma}</p>
+        <a href="#" class="btn btn-primary form-btn" id="home-btn-return">Finzalizar</a>`)
     return database.ref('users/' + USER_ID).update({
         name: nameEdit,
         lastName: lastNameEdit,
