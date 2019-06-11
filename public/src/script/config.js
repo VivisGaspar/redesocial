1329d200d879144cce934eb07d92583943d677e3
marinakeu <marina_keunecke@hotmail.com>
2019-06-11 14:00:15 -0300

    refatora - identação e exclusão de funções que não estavam sendo utilizadas

--- public/src/script/config.js
+++ public/src/script/config.js
@@ -1,9 +1,11 @@
 const config = {
-    apiKey: "AIzaSyC4-oq1Sj96_IJvy_wFxF7DoJRzDO5Lcks",
-    authDomain: "educalab-52ee0.firebaseapp.com",
-    databaseURL: "https://educalab-52ee0.firebaseio.com",
-    projectId: "educalab-52ee0",
-    storageBucket: "educalab-52ee0.appspot.com",
-    messagingSenderId: "503327975641"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyC4-oq1Sj96_IJvy_wFxF7DoJRzDO5Lcks",
+  authDomain: "educalab-52ee0.firebaseapp.com",
+  databaseURL: "https://educalab-52ee0.firebaseio.com",
+  projectId: "educalab-52ee0",
+  storageBucket: "educalab-52ee0.appspot.com",
+  messagingSenderId: "503327975641"
+};
+
+firebase.initializeApp(config);
+const database = firebase.database();
